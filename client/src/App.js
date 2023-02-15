cb4fb2d5b6761ae298a2e5c3ab79814e8df1ff6d
Jacqueline Gellick <jgellick@gmail.com>
2023-02-15 15:59:28 +0100

    Added student inputs

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-//import React, { useEffect, useState } from "react";
+
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
@@ -11,14 +11,28 @@ function App() {
     annualDate: Date.now(),
     triennialDate: Date.now(),
     goal: "",
-    minutes: Date.now(),
+    minutes: 0,
   });
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addStudent();
+  };
+
+  const handleChange = e => {
+    setNewStudent({ ...newStudent, [e.target.name]: e.target.value });
+  };
 
   async function getStudents() {
     try {
       let results = await fetch("/students");
       let data = await results.json();
       console.log(data);
+      setStudents(data);
     } catch(err) {
       console.log(err);
     }
@@ -73,9 +87,9 @@ function App() {
     setNewStudent({
       firstname: "",
       lastname: "",
-      birthdate: 0,
-      annualDate: 0,
-      triennialDate: 0,
+      birthdate: Date.now(),
+      annualDate: Date.now(),
+      triennialDate: Date.now(),
       goal: "",
       minutes: 0,
     });
@@ -98,6 +112,65 @@ function App() {
   return (
     <div className="App">
       <h1>Speech therapy app</h1>
+      <form onSubmit={e => handleSubmit(e)}>
+          <input 
+            type="text"
+            name="firstname"
+            placeholder="First Name"
+            onChange={e => handleChange(e)}
+            value={newStudent.firstname} 
+            />        
+          <input 
+            type="text"
+            name="lastname"
+            placeholder="Last Name"
+            onChange={e => handleChange(e)}
+            value={newStudent.lastname} 
+            />        
+          <input 
+            type="date"
+            name="birthdate"
+            placeholder="Birthdate"
+            onChange={e => handleChange(e)}
+            value={newStudent.birthdate}   
+            />      
+          <input 
+            type="date"
+            name="annualDate"
+            placeholder="Annual Review Date"
+            onChange={e => handleChange(e)}
+            value={newStudent.annualDate}
+            />
+          <input 
+            type="date"
+            name="triennialDate"
+            placeholder="Triennial Reevaluation Date"
+            onChange={e => handleChange(e)}
+            value={newStudent.triennialDate} 
+            />            
+          <input 
+            type="number"
+            name="minutes"
+            placeholder="Minutes per month"
+            onChange={e => handleChange(e)}
+            value={newStudent.minutes} 
+            />            
+          <button type="Submit">Submit</button>
+      </form>
+      <div className="content">
+        <ul>
+          {students.map(student => {
+            <li key={student.id}>
+              <span>{`${student.firstname}`}</span>
+              <span>{`${student.lastname}`}</span>
+              <span>{`${student.birthdate}`}</span>
+              <span>{`${student.annualDate}`}</span>
+              <span>{`${student.triennialDate}`}</span>
+              <span>{`${student.minutes}`}</span>
+            </li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
