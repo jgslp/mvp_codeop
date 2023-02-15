206aa8b52a9a6108dd48894691e47de8d27538a6
Jacqueline Gellick <jgellick@gmail.com>
2023-02-15 11:17:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// you can delete if you want or just delete users and keep the index
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
