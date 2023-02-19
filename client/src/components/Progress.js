3212134c5bc20d56eac7bbb0190ff16a5fb3fb7e
Jacqueline Gellick <jgellick@gmail.com>
2023-02-19 02:46:32 +0100

    Changed inputs on Progress component to bootstrap

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -50,20 +50,46 @@ function Progress({students, getStudents}) {
                 )}
                 <form>
                     <div className="progress-inputs">
-                        <input type="date" className="session-input"></input>
-                        <select name="" id="" className="session-input">
-                            <option value="">{`High five, ${profileStudent.firstname}!`}</option>
-                            <option value="">{`Thanks for your hard work today, ${profileStudent.firstname}!`}</option>
-                            <option value="">{`Amazing job, ${profileStudent.firstname}!`}</option>
-                            <option value="">{`Outstanding effort, ${profileStudent.firstname}!`}</option>
-                            <option value="">{`Keep up the good work, ${profileStudent.firstname}!`}</option>
-                            <option value="">{`${profileStudent.firstname} had difficulty participating in speech today.`}</option>
-                            <option value="">{`${profileStudent.firstname} was unavailable for speech today.`}</option>
-                            <option value="">{`${profileStudent.firstname} was absent from school today.`}</option>
-                        </select>
-                        <input type="number" className="session-input" placeholder="% trials correct"></input>
-                        <textarea className="session-input" placeholder="Anecdotal data"></textarea>
-                        <textarea className="session-input" placeholder="Skill to practice at home"></textarea>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="basic-addon1">Session Date</span>
+                            </div>
+                            <input type="date" className="session-input form-control"></input>
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <label className="input-group-text" for="inputGroupSelect01">Options</label>
+                            </div>
+                            <select name="" id="" className="session-input form-control">
+                                <option value="">Choose...</option>
+                                <option value="">{`High five, ${profileStudent.firstname}!`}</option>
+                                <option value="">{`Thanks for your hard work today, ${profileStudent.firstname}!`}</option>
+                                <option value="">{`Amazing job, ${profileStudent.firstname}!`}</option>
+                                <option value="">{`Outstanding effort, ${profileStudent.firstname}!`}</option>
+                                <option value="">{`Keep up the good work, ${profileStudent.firstname}!`}</option>
+                                <option value="">{`${profileStudent.firstname} had difficulty participating in speech today.`}</option>
+                                <option value="">{`${profileStudent.firstname} was unavailable for speech today.`}</option>
+                                <option value="">{`${profileStudent.firstname} was absent from school today.`}</option>
+                            </select>
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="basic-addon1">% trials correct</span>
+                            </div>
+                            <input type="number" className="session-input form-control"></input>
+                        </div>
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text textarea-span">Anecdotal data</span>
+                            </div>
+                            <textarea className="session-input form-control" ></textarea>
+                        </div>
+                        <div className="input-group">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text textarea-span">Skill to practice at home</span>
+                            </div>
+                            <textarea className="session-input form-control" ></textarea>
+                        </div>
                     </div>
                     <button type="submit" className="btn btn-primary">Add session</button>
                 </form>
