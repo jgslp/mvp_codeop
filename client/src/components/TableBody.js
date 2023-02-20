3dfda0785854e7e29d661b2c1f0dca62b2170a05
Jacqueline Gellick <jgellick@gmail.com>
2023-02-20 16:19:42 +0100

    Trimmed timestamp from dates

--- client/src/components/TableBody.js
+++ client/src/components/TableBody.js
@@ -8,9 +8,9 @@ function TableBody({ tableData, deleteStudent}) {
         <tr key={data.id}>
             <td>{data.firstname ? data.firstname : "——"}</td>
             <td>{data.lastname ? data.lastname : "——"}</td>
-            <td>{data.birthdate ? data.birthdate : "——"}</td>
-            <td>{data.annual ? data.annual : "——"}</td>
-            <td>{data.triennial ? data.triennial : "——"}</td>
+            <td>{data.birthdate ? data.birthdate.toString().substring(0, 10) : "——"}</td>
+            <td>{data.annual ? data.annual.toString().substring(0, 10) : "——"}</td>
+            <td>{data.triennial ? data.triennial.toString().substring(0, 10) : "——"}</td>
             <td>{data.minutes ? data.minutes : "——"}</td>
             <td><span className="material-symbols-outlined" onClick={() => deleteStudent(data.id)}>delete</span></td>
         </tr>
