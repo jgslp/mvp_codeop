140d446ab51056e1f2e2e49713203e9984fa92e7
Jacqueline Gellick <jgellick@gmail.com>
2023-02-23 13:10:31 +0100

    Changed spacing for Progress component

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -37,40 +37,46 @@ function Progress({students, getStudents}) {
       }
 
     return (
-        <div className="progress-view">
-            <Sidebar students={students} viewProfile={viewProfile} />
-            <div className="profile-section">
-                {profile && (
-                    <div>
-                        <h3>{`${profileStudent.firstname} ${profileStudent.lastname}`}</h3>
-                        <h6>{profileStudent.goal}</h6>
+        <div className="progress-view row">
+            <div className="col-2 offset-2">
+                <Sidebar students={students} viewProfile={viewProfile} />
+            </div>
+            <div className="col-6 content">
+                <div className="profile-section">
+                    {profile && (
+                        <div>
+                            <h3>{`${profileStudent.firstname} ${profileStudent.lastname}`}</h3>
+                            <h6>{profileStudent.goal}</h6>
+                        </div>
+                    )}
+                    <div className="progress-form">
+                        {profile && (
+                            < ProgressForm  profileStudent={profileStudent} getSessions={getSessions}/> 
+                        )}
                     </div>
-                )}
-                {profile && (
-                    < ProgressForm  profileStudent={profileStudent} getSessions={getSessions}/> 
-                )}
-                <div>  
-                    {/* most recent session at top */}
-                    {sessions.slice(0).reverse().map((data) => {
-                        // if student_id from sessions === selected student's id
-                        if (data.student_id === profileStudent.id) {
-                            return (
-                            <div key={data.id} className="card bg-light mb-3 session-card">
-                                <div className="card-header session-header d-flex justify-content-end">{data.sessionDate.toString().substring(0, 10)}</div>
-                                <div className="card-body">
-                                    <h5 className="card-title">{data.attendance}</h5>
-                                <div>{data.anecdote}</div>
-                                {data.trials !== 0 && (
-                                    <div>{`Accuracy: ${data.trials}%`}</div>
-                                )}
-                                {data.homework && (
-                                    <div>Homework: {data.homework}</div>
-                                )}
+                    <div>  
+                        {/* most recent session at top */}
+                        {sessions.slice(0).reverse().map((data) => {
+                            // if student_id from sessions === selected student's id
+                            if (data.student_id === profileStudent.id) {
+                                return (
+                                <div key={data.id} className="card bg-light mb-3 session-card">
+                                    <div className="card-header session-header d-flex justify-content-end">{data.sessionDate.toString().substring(0, 10)}</div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{data.attendance}</h5>
+                                    <div>{data.anecdote}</div>
+                                    {data.trials !== 0 && (
+                                        <div>{`Accuracy: ${data.trials}%`}</div>
+                                    )}
+                                    {data.homework && (
+                                        <div>Homework: {data.homework}</div>
+                                    )}
+                                    </div>
                                 </div>
-                            </div>
-                     );
-                }
-            })}
+                        );
+                    }
+                })}
+            </div>
         </div>
                
             </div>
